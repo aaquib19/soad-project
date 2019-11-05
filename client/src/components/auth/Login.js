6dc96cb3a1a01619610d49b2881d94679ffefae2
wilson <wilson.p17@iiits.in>
2019-11-05 06:01:20 +0530

    removed some warning

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,19 +1,13 @@
-import React, { Component, Fragment } from "react";
-import { makeStyles, createMuiTheme } from "@material-ui/core/styles";
+import React, { Component } from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider, withStyles } from "@material-ui/styles";
-// import TextField from "@material-ui/core/TextField";
-import { green, blue } from "@material-ui/core/colors";
-// import Button from "@material-ui/core/Button";
+import { blue } from "@material-ui/core/colors";
 import { connect } from "react-redux";
-import PropTypes from "prop-types";
 import { LoginUser } from "../../actions/authAction";
-// import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
+
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
