144eb5f5c26acb6a75b3c9017ec234c01f9e96c6
wilson <wilson.p17@iiits.in>
2019-11-06 20:53:18 +0530

    modified the 404 error page

--- client/src/components/layout/errorPage.jsx
+++ client/src/components/layout/errorPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import logo from "./404.png";
+import logo from "./404.jpg";
 import NavBar1 from "./Timeline/nav";
+import { Link } from "react-router-dom";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 const ErrorPage = () => {
   return (
     <React.Fragment>
@@ -8,9 +10,25 @@ const ErrorPage = () => {
       <br />
       <br />
       <br />
-      <div style={{ margin: "5%", height: "70%", width: "70%" }}>
-        <img src={logo} style={{ height: "50%", widht: "50%" }} />
+      <div style={{ marginTop: "1%", marginBottom: "2rem" }}>
+        <img src={logo} />
       </div>
+      <h1
+        style={{ fontFamily: "Agency FB", fontSize: "80px", color: "#6d6e71" }}
+      >
+        Whoops !!!
+      </h1>
+      <h3
+        style={{ fontFamily: "Agency FB", fontSize: "30px", color: "#6d6e71" }}
+      >
+        Looks like you are lost. Lets go back.
+      </h3>
+      <Link to="/success">
+        <button className="btn btn-secondary">
+          <ArrowBackIcon />
+          Go Back
+        </button>
+      </Link>
     </React.Fragment>
   );
 };
