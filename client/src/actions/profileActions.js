a051a75b85bf5052311a0fc8af3078de0dabdfe3
Aaquib Niaz <aaquibniaz3600@gmail.com>
2019-11-16 13:27:21 +0530

    corrected redirect

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -31,7 +31,7 @@ export const getCurrentProfile = () => dispatch => {
 export const createProfile = (profileData, history) => dispatch => {
   axios
     .post("/api/profile", profileData)
-    .then(res => history.push("/dashboard"))
+    .then(res => history.push("/profile"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
