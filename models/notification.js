8c862b1f3d3281af985c02a0a50b836758b26577
Rahul Prasad <1rahulprasad1@gmail.com>
2019-12-07 16:16:20 +0530

    update notification model

--- models/notification.js
+++ models/notification.js
@@ -2,32 +2,28 @@ const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema(
 {
-    friend_requests_unseen: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "users"
-        }
-    ],
-    friend_requests_seen: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "users"
-        }
-    ],
-    posts_unseen: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "post"
-        }
-    ],
-    posts_seen: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "post"
-        }
-    ]
-},
-    { timestamps: true }
-);
+    type_of_notification: String,
+    seen: Boolean,
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: "post"
+    },
+    who_did: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    to_whom: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    friend: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
 
 module.exports = mongoose.model("Notification", notificationSchema);
