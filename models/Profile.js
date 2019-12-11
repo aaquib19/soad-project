42768280b86e897e605270ca3d1cc1f5f7335547
Aaquib Niaz <aaquibniaz3600@gmail.com>
2019-12-12 04:07:42 +0530

    edit profile working

--- models/Profile.js
+++ models/Profile.js
@@ -22,8 +22,8 @@ const ProfileSchema = new Schema({
     type: String
   },
   status: {
-    type: String,
-    required: true
+    type: String
+    // required: true
   },
   skills: {
     type: [[String]],
